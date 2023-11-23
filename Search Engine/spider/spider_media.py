00e582840221342ba1e3c8ead13a8b877c5235a9
cmh <cmh1447283266@gmail.com>
2023-11-23 16:49:36 +0800

    update

--- Search Engine/spider/spider_media.py
+++ Search Engine/spider/spider_media.py
@@ -6,14 +6,15 @@ import httpx
 from parsel import Selector
 
 url_list = [f'https://news.nankai.edu.cn/mtnk/system/count//0006000/000000000000/000/000/c0006000000000000000_000000{i}.shtml' for i in range(750,903)]
+url_list.append("https://news.nankai.edu.cn/mtnk/index.shtml")
 
-sem = asyncio.Semaphore(5)  # 设置协程数，爬虫协程限制较低，减少被爬服务器的压力
+sem = asyncio.Semaphore(12)  # 设置协程数，爬虫协程限制较低，减少被爬服务器的压力
 asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy()) # 解决windows下的RuntimeError: This event loop is already running
 
-result_dict = {}
+url_dict = {}
 
-title_url_df = pd.DataFrame(columns=['url'])
-title_url_df.index.name = 'title'
+url_df = pd.DataFrame(columns=['url'])
+url_df.index.name = 'title'
 
 async def parse_catalogs_page(url):
     async with sem:
@@ -28,8 +29,7 @@ async def parse_catalogs_page(url):
                     response = await client.get(redirect_url)
             selector = Selector(response.text)
             print(response.text)
-            temp_dict = zip(selector.css('a::attr(href)').getall(), selector.css('a::text').getall())
-            result_dict.update(temp_dict)
+            url_dict.update(zip(selector.css('a::attr(href)').getall(), selector.css('a::text').getall()))
 
 
 async def parse_page(url):
@@ -45,9 +45,9 @@ async def parse_page(url):
                     try:
                         if "/" in title:
                             title = title.replace("/", "_")
-                        async with aiofiles.open(f'./nk_media/{title}.html', mode='w', encoding='utf-8') as f:
-                            await f.write(response.text)
-                        title_url_df.loc[title] = url  # 插入df
+                        async with aiofiles.open(f'./nk_media/{title}.html', mode='w', encoding='utf-8') as file:
+                            await file.write(response.text)
+                        url_df.loc[title] = url  # 插入df
                     # 爬取的网页存在问题
                     except Exception as e:
                         print(f'{e}: {url}|{title}')
@@ -61,12 +61,12 @@ async def main():
 
     tasks = [asyncio.create_task(parse_catalogs_page(url)) for url in url_list]
     await asyncio.gather(*tasks)
-    print(result_dict.keys())
-    tasks = [asyncio.create_task(parse_page(url)) for url in result_dict.keys()]
+
+    tasks = [asyncio.create_task(parse_page(url)) for url in url_dict.keys()]
     await asyncio.gather(*tasks)
 
     # 写入文件
-    title_url_df.to_csv("./nk_media.csv")
+    url_df.to_csv("./nk_media.csv")
 
 
 if __name__ == '__main__':
--- Search Engine/spider/spider_news.py
+++ Search Engine/spider/spider_news.py
@@ -9,17 +9,15 @@ from parsel import Selector
 url_list = [f'http://news.nankai.edu.cn/ywsd/system/count//0003000/000000000000/000/000/c0003000000000000000_000000{i}.shtml' for i in range(497, 604)]
 url_list.append('http://news.nankai.edu.cn/ywsd/index.shtml')
 
-sem = asyncio.Semaphore(5)  # 设置协程数，爬虫协程限制较低，减少被爬服务器的压力
+coroutine = asyncio.Semaphore(5)  # 设置协程数，爬虫协程限制较低，减少被爬服务器的压力
 asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy()) # 解决windows下的RuntimeError: This event loop is already running
 
-result_dict = {}
+url_dict = {}
+url_df = pd.DataFrame(columns=['url'])
+url_df.index.name = 'title'
 
-# 创建title_url_df，用于存储title和url，以便后续使用，索引是title，列是url
-title_url_df = pd.DataFrame(columns=['url'])
-title_url_df.index.name = 'title'
-
-async def parse_catalogs_page(url):
-    async with sem:
+async def parse_catalogs(url):
+    async with coroutine:
         async with httpx.AsyncClient() as client:
             response = await client.get(url)
             if response.status_code == 302:
@@ -29,13 +27,12 @@ async def parse_catalogs_page(url):
                     # 使用新的 URL 发起请求
                     response = await client.get(redirect_url)
             selector = Selector(response.text)
-            print(response.text)
-            temp_dict = zip(selector.css('a::attr(href)').getall(), selector.css('a::text').getall())
-            result_dict.update(temp_dict)
+            print(response.text) # 打印爬取到的网页结构
+            url_dict.update(zip(selector.css('a::attr(href)').getall(), selector.css('a::text').getall()))
 
 
 async def parse_page(url):
-    async with sem:
+    async with coroutine:
         print("正在获取..."+str(url))
         try:
             async with httpx.AsyncClient(follow_redirects=True, timeout=10,
@@ -47,9 +44,9 @@ async def parse_page(url):
                     try:
                         if "/" in title:
                             title = title.replace("/", "_")
-                        async with aiofiles.open(f'./nk_news/{title}.html', mode='w', encoding='utf-8') as f:
-                            await f.write(response.text)
-                        title_url_df.loc[title] = url  # 插入df
+                        async with aiofiles.open(f'./nk_news/{title}.html', mode='w', encoding='utf-8') as file:
+                            await file.write(response.text)
+                        url_df.loc[title] = url  # 插入df
                     # 爬取的网页存在问题
                     except Exception as e:
                         print(f'{e}: {url}|{title}')
@@ -61,14 +58,14 @@ async def main():
     if not os.path.exists('./nk_news'):
         os.mkdir('./nk_news')
 
-    tasks = [asyncio.create_task(parse_catalogs_page(url)) for url in url_list]
+    tasks = [asyncio.create_task(parse_catalogs(url)) for url in url_list]
     await asyncio.gather(*tasks)
 
-    tasks = [asyncio.create_task(parse_page(url)) for url in result_dict.keys()]
+    tasks = [asyncio.create_task(parse_page(url)) for url in url_dict.keys()]
     await asyncio.gather(*tasks)
 
     # 写入文件
-    title_url_df.to_csv("./nk_news.csv")
+    url_df.to_csv("./nk_news.csv")
 
 
 if __name__ == '__main__':
